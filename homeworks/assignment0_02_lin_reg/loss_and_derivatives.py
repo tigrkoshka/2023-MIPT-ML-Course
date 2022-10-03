27e64e6ee8b99c4524caeda0efb2bc5cda784c9d
Tigran <tigrankoshkelyan@gmail.com>
2022-10-03 21:53:35 +0300

    HW2 pretty

--- homeworks/assignment0_02_lin_reg/loss_and_derivatives.py
+++ homeworks/assignment0_02_lin_reg/loss_and_derivatives.py
@@ -6,30 +6,30 @@ class LossAndDerivatives:
     def mse(X, Y, w):
         """
         X : numpy array of shape (`n_observations`, `n_features`)
-        Y : numpy array of shape (`n_observations`, `target_dimentionality`) or (`n_observations`,)
-        w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
+        Y : numpy array of shape (`n_observations`, `target_dimensionality`) or (`n_observations`,)
+        w : numpy array of shape (`n_features`, `target_dimensionality`) or (`n_features`,)
 
         Return : float
             single number with MSE value of linear model (X.dot(w)) with no bias term
             on the selected dataset.
         
-        Comment: If Y is two-dimentional, average the error over both dimentions.
+        Comment: If Y is two-dimensional, average the error over both dimensions.
         """
 
-        return np.mean((X.dot(w) - Y)**2)
+        return np.mean((X.dot(w) - Y) ** 2)
 
     @staticmethod
     def mae(X, Y, w):
         """
         X : numpy array of shape (`n_observations`, `n_features`)
-        Y : numpy array of shape (`n_observations`, `target_dimentionality`) or (`n_observations`,)
-        w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
+        Y : numpy array of shape (`n_observations`, `target_dimensionality`) or (`n_observations`,)
+        w : numpy array of shape (`n_features`, `target_dimensionality`) or (`n_features`,)
                 
         Return: float
             single number with MAE value of linear model (X.dot(w)) with no bias term
             on the selected dataset.
 
-        Comment: If Y is two-dimentional, average the error over both dimentions.
+        Comment: If Y is two-dimensional, average the error over both dimensions.
         """
 
         return np.mean(np.abs(X.dot(w) - Y))
@@ -37,7 +37,7 @@ class LossAndDerivatives:
     @staticmethod
     def l2_reg(w):
         """
-        w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
+        w : numpy array of shape (`n_features`, `target_dimensionality`) or (`n_features`,)
 
         Return: float
             single number with sum of squared elements of the weight matrix ( \sum_{ij} w_{ij}^2 )
@@ -50,7 +50,7 @@ class LossAndDerivatives:
     @staticmethod
     def l1_reg(w):
         """
-        w : numpy array of shape (`n_features`, `target_dimentionality`)
+        w : numpy array of shape (`n_features`, `target_dimensionality`)
 
         Return : float
             single number with sum of the absolute values of the weight matrix ( \sum_{ij} |w_{ij}| )
@@ -66,20 +66,20 @@ class LossAndDerivatives:
         Simply ignores the regularization
         """
         return 0.
-    
+
     @staticmethod
     def mse_derivative(X, Y, w):
         """
         X : numpy array of shape (`n_observations`, `n_features`)
-        Y : numpy array of shape (`n_observations`, `target_dimentionality`) or (`n_observations`,)
-        w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
+        Y : numpy array of shape (`n_observations`, `target_dimensionality`) or (`n_observations`,)
+        w : numpy array of shape (`n_features`, `target_dimensionality`) or (`n_features`,)
         
         Return : numpy array of same shape as `w`
 
         Computes the MSE derivative for linear regression (X.dot(w)) with no bias term
         w.r.t. w weight matrix.
         
-        Please mention, that in case `target_dimentionality` > 1 the error is averaged along this
+        Please mention, that in case `target_dimensionality` > 1 the error is averaged along this
         dimension as well, so you need to consider that fact in derivative implementation.
         """
 
@@ -89,15 +89,15 @@ class LossAndDerivatives:
     def mae_derivative(X, Y, w):
         """
         X : numpy array of shape (`n_observations`, `n_features`)
-        Y : numpy array of shape (`n_observations`, `target_dimentionality`) or (`n_observations`,)
-        w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
+        Y : numpy array of shape (`n_observations`, `target_dimensionality`) or (`n_observations`,)
+        w : numpy array of shape (`n_features`, `target_dimensionality`) or (`n_features`,)
         
         Return : numpy array of same shape as `w`
 
         Computes the MAE derivative for linear regression (X.dot(w)) with no bias term
         w.r.t. w weight matrix.
         
-        Please mention, that in case `target_dimentionality` > 1 the error is averaged along this
+        Please mention, that in case `target_dimensionality` > 1 the error is averaged along this
         dimension as well, so you need to consider that fact in derivative implementation.
         """
 
@@ -106,7 +106,7 @@ class LossAndDerivatives:
     @staticmethod
     def l2_reg_derivative(w):
         """
-        w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
+        w : numpy array of shape (`n_features`, `target_dimensionality`) or (`n_features`,)
 
         Return : numpy array of same shape as `w`
 
@@ -118,8 +118,8 @@ class LossAndDerivatives:
     @staticmethod
     def l1_reg_derivative(w):
         """
-        Y : numpy array of shape (`n_observations`, `target_dimentionality`) or (`n_observations`,)
-        w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
+        Y : numpy array of shape (`n_observations`, `target_dimensionality`) or (`n_observations`,)
+        w : numpy array of shape (`n_features`, `target_dimensionality`) or (`n_features`,)
 
         Return : numpy array of same shape as `w`
 
