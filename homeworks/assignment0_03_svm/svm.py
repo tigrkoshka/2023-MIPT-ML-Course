5cc94d87b3b831c1f33c6586256456512b19ffd7
Tigran <tigrankoshkelyan@gmail.com>
2022-10-06 20:12:13 +0300

    HW3 pretty

--- homeworks/assignment0_03_svm/svm.py
+++ homeworks/assignment0_03_svm/svm.py
@@ -1,14 +1,12 @@
 from sklearn.base import BaseEstimator, ClassifierMixin
-from sklearn.metrics import accuracy_score
 
 import numpy as np
 import torch
-import torch.nn as nn
 import torch.optim as optim
 
 
 def rbf(x_1, x_2, sigma=1.):
-    '''Computes rbf kernel for batches of objects
+    """Computes rbf kernel for batches of objects
 
     Args:
         x_1: torch.tensor shaped `(#samples_1, #features)` of type torch.float32
@@ -16,16 +14,17 @@ def rbf(x_1, x_2, sigma=1.):
     Returns:
         kernel function values for all pairs of samples from x_1 and x_2
         torch.tensor of type torch.float32 shaped `(#samples_1, #samples_2)`
-    '''
+    """
     x_1_2 = (x_1 ** 2).sum(axis=1).reshape(x_1.shape[0], 1)
     x_2_2 = (x_2 ** 2).sum(axis=1).reshape(1, x_2.shape[0])
     x_1_x_2 = x_1 @ x_2.T
     distances = torch.exp(-(x_1_2 + x_2_2 - 2 * x_1_x_2) / (2 * sigma ** 2))
     return torch.Tensor(distances).type(torch.float32)
 
+
 def hinge_loss(scores, labels):
-    '''Mean loss for batch of objects
-    '''
+    """Mean loss for batch of objects
+    """
     assert len(scores.shape) == 1
     assert len(labels.shape) == 1
     return torch.mean(torch.max(torch.zeros(scores.shape[0]), 1 - scores * labels))
@@ -34,26 +33,29 @@ def hinge_loss(scores, labels):
 class SVM(BaseEstimator, ClassifierMixin):
     @staticmethod
     def linear(x_1, x_2):
-        '''Computes linear kernel for batches of objects
-        
+        """Computes linear kernel for batches of objects
+
         Args:
             x_1: torch.tensor shaped `(#samples_1, #features)` of type torch.float32
             x_2: torch.tensor shaped `(#samples_1, #features)` of type torch.float32
         Returns:
             kernel function values for all pairs of samples from x_1 and x_2
             torch.tensor shaped `(#samples_1, #samples_2)` of type torch.float32
-        '''
+        """
         return x_1 @ x_2.T
-    
+
     def __init__(
-        self,
-        lr: float=1e-3,
-        epochs: int=2,
-        batch_size: int=64,
-        lmbd: float=1e-4,
-        kernel_function=None,
-        verbose: bool=False,
+            self,
+            lr: float = 1e-3,
+            epochs: int = 2,
+            batch_size: int = 64,
+            lmbd: float = 1e-4,
+            kernel_function=None,
+            verbose: bool = False,
     ):
+        self.X = None
+        self.bias = None
+        self.betas = None
         self.lr = lr
         self.epochs = epochs
         self.batch_size = batch_size
@@ -72,30 +74,30 @@ class SVM(BaseEstimator, ClassifierMixin):
         K = self.kernel_function(X, X).float()
 
         self.betas = torch.full((n_obj, 1), fill_value=0.001, dtype=X.dtype, requires_grad=True)
-        self.bias = torch.zeros(1, requires_grad=True) # I've also add bias to the model
-        
+        self.bias = torch.zeros(1, requires_grad=True)  # I've also add bias to the model
+
         optimizer = optim.SGD((self.betas, self.bias), lr=self.lr)
         for epoch in range(self.epochs):
             perm = torch.randperm(n_obj)  # Generate a set of random numbers of length: sample size
-            sum_loss = 0.                 # Loss for each epoch
+            sum_loss = 0.  # Loss for each epoch
             for i in range(0, n_obj, self.batch_size):
                 batch_inds = perm[i:i + self.batch_size]
-                x_batch = X[batch_inds]   # Pick random samples by iterating over random permutation
-                y_batch = Y[batch_inds]   # Pick the correlating class
+                y_batch = Y[batch_inds]  # Pick the correlating class
                 k_batch = K[batch_inds]
-                
-                optimizer.zero_grad()     # Manually zero the gradient buffers of the optimizer
+
+                optimizer.zero_grad()  # Manually zero the gradient buffers of the optimizer
 
                 # get the matrix product using SVM parameters: self.betas and self.bias
                 preds = k_batch @ self.betas + self.bias
                 preds = preds.flatten()
                 loss = self.lmbd * self.betas[batch_inds].T @ k_batch @ self.betas + hinge_loss(preds, y_batch)
-                loss.backward()           # Backpropagation
-                optimizer.step()          # Optimize and adjust weights
+                loss.backward()  # Backpropagation
+                optimizer.step()  # Optimize and adjust weights
 
-                sum_loss += loss.item()   # Add the loss
+                sum_loss += loss.item()  # Add the loss
 
-            if self.verbose: print("Epoch " + str(epoch) + ", Loss: " + str(sum_loss / self.batch_size))
+            if self.verbose:
+                print("Epoch " + str(epoch) + ", Loss: " + str(sum_loss / self.batch_size))
 
         self.X = X
         self.fitted = True
