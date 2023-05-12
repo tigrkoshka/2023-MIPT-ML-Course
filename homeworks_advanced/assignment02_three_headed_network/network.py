b50a02966f954a78aae6cd8d750aab7f4020f1b1
Tigran <tigrankoshkelyan@gmail.com>
2023-05-12 15:05:48 +0300

    Assignment 2

--- homeworks_advanced/assignment02_three_headed_network/network.py
+++ homeworks_advanced/assignment02_three_headed_network/network.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 
@@ -12,39 +11,52 @@ import tqdm
 class ThreeInputsNet(nn.Module):
     def __init__(self, n_tokens, n_cat_features, concat_number_of_features, hid_size=64):
         super(ThreeInputsNet, self).__init__()
+
         self.title_emb = nn.Embedding(n_tokens, embedding_dim=hid_size)
-        # <YOUR CODE HERE>        
-        
+        self.title = nn.Sequential(
+            nn.Conv1d(in_channels=hid_size, out_channels=hid_size * 2, kernel_size=3),
+            nn.ReLU(),
+            nn.AdaptiveMaxPool1d(1),
+            nn.Flatten(),
+        )
+
         self.full_emb = nn.Embedding(num_embeddings=n_tokens, embedding_dim=hid_size)
-        # <YOUR CODE HERE>
-        
-        self.category_out = # <YOUR CODE HERE>
+        self.full = nn.Sequential(
+            nn.Conv1d(in_channels=hid_size, out_channels=hid_size * 2, kernel_size=3),
+            nn.ReLU(),
+            nn.AdaptiveMaxPool1d(1),
+            nn.Flatten(),
+        )
 
+        self.category_out = nn.Sequential(
+            nn.Linear(in_features=n_cat_features, out_features=hid_size * 2),
+            nn.ReLU(),
+        )
 
-        # Example for the final layers (after the concatenation)
-        self.inter_dense = nn.Linear(in_features=concat_number_of_features, out_features=hid_size*2)
-        self.final_dense = nn.Linear(in_features=hid_size*2, out_features=1)
-
-        
+        self.dense = nn.Sequential(
+            nn.Linear(in_features=concat_number_of_features, out_features=hid_size * 2),
+            nn.Linear(in_features=hid_size * 2, out_features=1),
+        )
 
     def forward(self, whole_input):
         input1, input2, input3 = whole_input
+
         title_beg = self.title_emb(input1).permute((0, 2, 1))
-        title = # <YOUR CODE HERE>
-        
+        title = self.title(title_beg)
+
         full_beg = self.full_emb(input2).permute((0, 2, 1))
-        full = # <YOUR CODE HERE>        
-        
-        category = # <YOUR CODE HERE>        
-        
+        full = self.full(full_beg)
+
+        category = self.category_out(input3)
+
         concatenated = torch.cat(
             [
-            title.view(title.size(0), -1),
-            full.view(full.size(0), -1),
-            category.view(category.size(0), -1)
+                title.view(title.size(0), -1),
+                full.view(full.size(0), -1),
+                category.view(category.size(0), -1)
             ],
             dim=1)
-        
-        out = # <YOUR CODE HERE>
-        
+
+        out = self.dense(concatenated)
+
         return out
